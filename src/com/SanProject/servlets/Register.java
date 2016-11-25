92e83301cdc1bbce5b6d0a5131e7b968df5ebb10
Shalom Nyende <shalom.nyende@gmail.com>
2016-11-25 09:45:56 +0300

    Tweaks made to the Register Servlet

--- src/com/SanProject/servlets/Register.java
+++ src/com/SanProject/servlets/Register.java
@@ -10,6 +10,7 @@ import java.sql.Statement;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -47,19 +48,22 @@ public class Register extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		response.setContentType("text/html");
 		String first_name, last_name, faculty, department, doctor, gender, regNumber;
-		first_name = request.getParameter("firstName").toString();
-		last_name = request.getParameter("lastName").toString();
-		faculty = request.getParameter("faculty").toString();
-		department = request.getParameter("department").toString();
-		doctor = request.getParameter("doctor").toString();
-		gender = request.getParameter("gender").toString();
-		regNumber = request.getParameter("regNumber").toString();
+		first_name = request.getParameter("firstName");
+		last_name = request.getParameter("lastName");
+		faculty = request.getParameter("faculty");
+		department = request.getParameter("department");
+		doctor = request.getParameter("doctor");
+		gender = request.getParameter("gender");
+		regNumber = request.getParameter("regNumber");
 		Connection conn = null;
 		try {
+//			HttpSession session = request.getSession(false);
+			Cookie[] cookie = request.getCookies();
+			
 			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection("jdbc:mysql://localhost/Stude",
 					"root", " ");
-			out.print("Success");
+			out.print("Connected to DB Successfully \n");
 			Statement sqlState = conn.createStatement();
 			String queryStatement = "insert into Queue values('"
 					+ regNumber + "','" + first_name + "','" + last_name
@@ -67,15 +71,23 @@ public class Register extends HttpServlet {
 					+ "','" + doctor + "')";
 			sqlState.executeUpdate(queryStatement);
 			// response.sendRedirect("Register.jsp");
+			if(cookie!=null){
+				String name = cookie[0].getValue();
+				if (!name.equals("")||name!=null){
 			RequestDispatcher dispatcher = request
 					.getRequestDispatcher("Register.jsp");
 			dispatcher.forward(request, response);
-			System.out.println("Registered Successfully");
+			out.println("Registered Successfully");
+			}
+			}
+			else{
+				out.println("Login first");
+			}
 
 		} catch (SQLException | ClassNotFoundException e) {
 			out.println("SQLException:" + e.getMessage());
 		}
-
+		
 	}
 
 }
